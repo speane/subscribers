2d51c2edd0c3a80b655db50bea36c7454397ac5e
Evgeny Shilov <v-evgeny.s@turvo.com>
2019-10-27 15:25:06 +0300

    Generate project

---
+++ src/main/java/dev/eshilov/nexigntask/NexigntaskApplication.java
@@ -0,0 +1,13 @@
+package dev.eshilov.nexigntask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexigntaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexigntaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/eshilov/nexigntask/NexigntaskApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.eshilov.nexigntask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexigntaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
